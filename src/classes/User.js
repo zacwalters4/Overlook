ae1eeddbf2150f442d0317eecf9d2b3003575fe3
Zac Walters <zacwalters4@gmail.com>
2022-11-13 20:50:12 -0700

    Create and complete basic user class tests

--- src/classes/User.js
+++ src/classes/User.js
@@ -2,10 +2,12 @@ class User {
     constructor(user, allBookings) {
         this.id = user.id
         this.name = user.name
-        this.bookings = getUserBookings(allBookings)
+        this.bookings = this.getUserBookings(allBookings)
     }
     getUserBookings(allBookings) {
         const userBookings = allBookings.filter(booking => booking.userID === this.id)
         return userBookings
     }
 }
+
+module.exports = User
---
+++ test/User-test.js
@@ -0,0 +1,27 @@
+import chai from 'chai'
+const expect = chai.expect
+import { userData, bookingsData } from '../data/testData'
+import User from '../src/classes/User'
+
+describe('User', () => {
+    let user
+    beforeEach(() => {
+      user = new User(userData, bookingsData)
+    })
+
+  it('should be an instance of User', () => {
+    expect(user).to.be.an.instanceOf(User)
+  })
+
+  it('should be have an ID', () => {
+    expect(user.id).to.equal(1)
+  })
+
+  it('should be have a name', () => {
+    expect(user.name).to.equal('Leatha Ullrich')
+  })
+
+  it('should be have an array of the user\'s bookings', () => {
+    expect(user.bookings).to.deep.equal([{"id":"5fwrgu4i7k55hl6t8","userID":1,"date":"2022/02/05","roomNumber":12}, {"id":"5fwrgu4i7k55hl6x8","userID":1,"date":"2023/01/11","roomNumber":20}])
+  })
+})
